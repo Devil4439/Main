a998a68b2b47be3b86e55e358d9804740c3838ac
Devil4439 <dashrajat666@gmail.com>
2024-11-26 09:49:30 +0530

    Update Practice.py

--- Practice.py
+++ Practice.py
@@ -1,4 +1,4 @@
 def function (n):
   return 1 if (n==1 or n==0) else n*function (n-1)
 num = int(input("enter a number:"))
-print(f"the factorial of {num} is ", fact(num))
+print(f"the factorial of {num} is",function(num))
