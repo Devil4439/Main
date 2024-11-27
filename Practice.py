c92f418634d199449d04ab42e9375d3a759c986a
Devil4439 <dashrajat666@gmail.com>
2024-11-27 09:03:15 +0530

    Update Factorial1.py

--- Practice.py
+++ Practice.py
@@ -1,4 +1,14 @@
 def function (n):
-  return 1 if (n==1 or n==0) else n*function (n-1)
-num = int(input("enter a number:"))
-print(f"the factorial of {num} is",function(num))
+  if (n==1) or (n==0):
+    return 1
+  elif n<0:
+    return "NOT POSSIBLE"
+  else:
+    fact=1
+    for i in range(1,n+1):
+      fact*=i
+    return fact
+num=int(input("Enter a number:"))
+print(f"THE FACTORIAL OF {num} IS:",fact)
+              
+  
